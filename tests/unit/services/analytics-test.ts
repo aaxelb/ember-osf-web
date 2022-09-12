a16fa15f4bd5709dc6518b48ae428c115762bd79
Abram Booth <boothaa@gmail.com>
2022-09-12 13:16:52 -0400

    no-config osf-metrics

--- tests/unit/services/analytics-test.ts
+++ tests/unit/services/analytics-test.ts
@@ -1,12 +1,17 @@
+import { setupMirage } from 'ember-cli-mirage/test-support';
 import { setupTest } from 'ember-qunit';
 import { module, test } from 'qunit';
 
 module('Unit | Service | analytics', hooks => {
     setupTest(hooks);
+    setupMirage(hooks);
 
-    // Replace this with your real tests.
-    test('it exists', function(assert) {
+    test('trackPage', async function(assert) {
         const service = this.owner.lookup('service:analytics');
-        assert.ok(service);
+        assert.ok(server.schema.countedUsages.all().length === 0);
+        await service.trackPage();
+        assert.ok(server.schema.countedUsages.all().length === 1);
+        const savedCountedUsage = server.schema.countedUsages.first();
+        assert.ok(savedCountedUsage.pageviewInfo.page_url === document.URL);
     });
 });
