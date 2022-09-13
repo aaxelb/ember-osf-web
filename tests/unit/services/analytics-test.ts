b4b136da0f4a17a3819f055a4ca2a54bb3415e1c
Abram Booth <boothaa@gmail.com>
2022-09-13 09:26:35 -0400

    shh, a test!

--- tests/unit/services/analytics-test.ts
+++ tests/unit/services/analytics-test.ts
@@ -1,3 +1,4 @@
+import { settled } from '@ember/test-helpers';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import { setupTest } from 'ember-qunit';
 import { module, test } from 'qunit';
@@ -8,10 +9,11 @@ module('Unit | Service | analytics', hooks => {
 
     test('trackPage', async function(assert) {
         const service = this.owner.lookup('service:analytics');
-        assert.ok(server.schema.countedUsages.all().length === 0);
+        assert.strictEqual(server.schema.countedUsages.all().length, 0, 'start with no counted usage');
         await service.trackPage();
-        assert.ok(server.schema.countedUsages.all().length === 1);
+        await settled();
+        assert.strictEqual(server.schema.countedUsages.all().length, 1, 'end with one counted usage');
         const savedCountedUsage = server.schema.countedUsages.first();
-        assert.ok(savedCountedUsage.pageviewInfo.page_url === document.URL);
+        assert.strictEqual(savedCountedUsage.pageviewInfo.page_url, document.URL, 'correct url');
     });
 });
